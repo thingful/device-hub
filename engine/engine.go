77a1a6aff51530c2dd07bcb0c8e8607c592a5357
Mark deVilliers <markdevilliers@gmail.com>
2017-02-24 13:55:55 +0000

    port simple engine

---
+++ engine/engine.go
@@ -0,0 +1,127 @@
+package engine
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"bitbucket.org/tsetsova/decode-prototype/hub/expando"
+	"github.com/robertkrimen/otto"
+)
+
+var (
+	ScriptTimedOutErr = errors.New("script timed out")
+)
+
+func New() engine {
+	return engine{}
+}
+
+type engine struct{}
+
+func (e engine) Execute(input expando.Input, script expando.Script) (interface{}, error) {
+
+	var output otto.Value
+	var err error
+
+	if script.Input == expando.Raw {
+		env := map[string]interface{}{
+			"__input": input.Payload,
+		}
+		output, err = e.run(script.Contents, "decode(__input);", env, time.Second*1)
+
+	}
+	if script.Input == expando.CSV {
+
+		env, err := prepareCSV(input)
+
+		if err != nil {
+			return nil, err
+		}
+
+		output, err = e.run(script.Contents, "decode(__header, __lines);", env, time.Second*1)
+
+	}
+
+	if script.Input == expando.JSON {
+
+		env := map[string]interface{}{
+			"__input": string(input.Payload),
+		}
+		output, err = e.run(script.Contents, "decode( JSON.parse( __input ));", env, time.Second*1)
+
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	gov, err := output.Export()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return gov, nil
+}
+
+// run javascript engine in-process
+// TODO : think about tolerance to malicious input, fault tolerance
+func (engine) run(code, main string, env map[string]interface{}, timeout time.Duration) (val otto.Value, err error) {
+
+	vm := otto.New()
+
+	// load the environment
+	for key, val := range env {
+		vm.Set(key, val)
+	}
+
+	vm.Set("__log", func(call otto.FunctionCall) otto.Value {
+		log.Println(call.ArgumentList)
+		return otto.UndefinedValue()
+	})
+	vm.Run("console.log = __log")
+
+	defer func() {
+
+		if caught := recover(); caught != nil {
+			val = otto.Value{}
+			if caught == ScriptTimedOutErr {
+				err = ScriptTimedOutErr
+			} else {
+				err = fmt.Errorf("fatal error in function: %s", caught)
+			}
+		}
+	}()
+
+	vm.Interrupt = make(chan func(), 1)
+
+	go func() {
+		time.Sleep(timeout)
+		vm.Interrupt <- func() {
+			panic(ScriptTimedOutErr)
+		}
+	}()
+
+	return vm.Run(fmt.Sprintf("%s;\n %s", code, main))
+}
+
+func prepareCSV(input expando.Input) (map[string]interface{}, error) {
+
+	r := csv.NewReader(strings.NewReader(string(input.Payload)))
+
+	records, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO : think about max records or map/reducing it
+	return map[string]interface{}{
+		"__header": records[0],
+		"__lines":  records[1:],
+	}, nil
+
+}
---
+++ engine/engine_test.go
@@ -0,0 +1,97 @@
+package engine
+
+import (
+	"bytes"
+	"encoding/binary"
+	"testing"
+
+	"bitbucket.org/tsetsova/decode-prototype/hub/expando"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRawDecodeValid(t *testing.T) {
+
+	t.Parallel()
+
+	script := expando.Script{
+		Runtime: expando.Javascript,
+		Input:   expando.Raw,
+		Contents: `function decode (input) {
+				return {
+						'value' : ((input[0] << 8) | input[1]) / 100,
+					}
+			}`,
+	}
+
+	// create the input payload as a byte array
+	buf := &bytes.Buffer{}
+
+	// multiple * 100 to ensure non floating point values
+	initialValue := 22.33
+	binary.Write(buf, binary.BigEndian, int16(initialValue*100))
+
+	input := expando.Input{Payload: buf.Bytes()}
+
+	e := New()
+	result, err := e.Execute(input, script)
+
+	assert.Nil(t, err)
+
+	resultAsMap := result.(map[string]interface{})
+	value := resultAsMap["value"]
+
+	assert.Equal(t, initialValue, value)
+}
+
+func TestCSVDecodeValid(t *testing.T) {
+
+	t.Parallel()
+
+	script := expando.Script{
+		Runtime: expando.Javascript,
+		Input:   expando.CSV,
+		Contents: `function decode (header, lines) {
+				return {
+						'header' : header,
+						'lines' : lines
+					}
+			}`,
+	}
+
+	csv := "column1, column2\none, two\nthree, four\n five,six"
+	input := expando.Input{Payload: []byte(csv)}
+
+	e := New()
+	result, err := e.Execute(input, script)
+	assert.Nil(t, err)
+	resultAsMap := result.(map[string]interface{})
+
+	assert.Len(t, resultAsMap["header"], 2)
+	assert.Len(t, resultAsMap["lines"], 3)
+
+}
+
+func TestJSONDecodeValid(t *testing.T) {
+
+	t.Parallel()
+
+	script := expando.Script{
+		Runtime: expando.Javascript,
+		Input:   expando.JSON,
+		Contents: `function decode (input) {
+				return input
+			}`,
+	}
+
+	json := "{ \"a\" : 1}"
+	input := expando.Input{Payload: []byte(json)}
+
+	e := New()
+	result, err := e.Execute(input, script)
+
+	assert.Nil(t, err)
+	resultAsMap := result.(map[string]interface{})
+
+	assert.NotNil(t, resultAsMap["a"])
+	assert.Equal(t, resultAsMap["a"], float64(1))
+}
