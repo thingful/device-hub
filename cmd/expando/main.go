a5949907881d3cd3867c1ca97c77a646b76b1a78
Mark deVilliers <markdevilliers@gmail.com>
2017-02-24 14:48:31 +0000

    hard code an example output - input from the data.py file

--- cmd/expando/main.go
+++ cmd/expando/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/thingful/expando"
+	"github.com/thingful/expando/engine"
+)
 
 func main() {
-	fmt.Println("hello")
+
+	scripter := engine.New()
+
+	j := `{
+		"value": "30",
+		"deviceId": "23",
+		"createdAt": "1487941771000"
+	}`
+
+	input := expando.Input{Payload: []byte(j)}
+
+	script := expando.Script{
+		Runtime: expando.Javascript,
+		Input:   expando.JSON,
+		Contents: `function decode (input) {
+
+			// define the terms against the m3-lite ontology
+			input['@context'] = {
+                'm3-lite': 'http://purl.org/iot/vocab/m3-lite#'
+			}
+
+			// it is an air pollutant sensor
+			input['@type'] = "m3-lite:AirPollutantSensor"
+
+			// environment based
+			input['domain'] = {
+                "@type" : "m3-lite:Environment"
+            }
+
+			// TODO : what is the value, unit
+
+			return input
+			}`,
+	}
+
+	output, err := scripter.Execute(input, script)
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := json.MarshalIndent(output, "", "   ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bytes))
+
 }
