0486e5b01c5628e36712942abc1c969ca8bb717b
Mark deVilliers <markdevilliers@gmail.com>
2017-02-27 11:31:17 +0000

    tweak js

--- cmd/expando/decode.js
+++ cmd/expando/decode.js
@@ -1,23 +1,24 @@
 
-// decode will add ontology information to the input
+// decode adds ontology information to the input
 function decode (input) {
+
     // you can log to debug your script!
     //console.log("decode called")
 
-    // define the terms against the m3-lite ontology
-    // http://ontology.fiesta-iot.eu/ontologyDocs/fiesta-iot/doc
     input['@context'] = {
+        // define the terms against the m3-lite ontology
+        // http://ontology.fiesta-iot.eu/ontologyDocs/fiesta-iot/doc
         'm3-lite': 'http://purl.org/iot/vocab/m3-lite#',
-        'decode' : 'http://decode.xxx'
+        // add in a decode context for the unique id
+        'decode' : 'http://decode.xxx',
     }
 
     input['@id'] = "decode:/" + input['deviceId'] + ':' + input['createdAt']
 
-
     // it is an air pollutant sensor
     input['@type'] = "m3-lite:AirPollutantSensor"
 
-    // environment based
+    // in the environment domain
     input['domain'] = {
     "@type" : "m3-lite:Environment"
     }
