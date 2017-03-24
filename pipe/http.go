1510ddb74f8cf81ad7303239258ea9224776c3de
Mark deVilliers <markdevilliers@gmail.com>
2017-03-24 16:51:17 +0000

    unit test the http handler

--- pipe/http.go
+++ pipe/http.go
@@ -11,30 +11,7 @@ import (
 
 func StartDefaultHTTPListener(ctx context.Context, router *router, binding string) {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		path := r.URL.Path
-
-		ok, channel := router.Match(path)
-
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		body, err := ioutil.ReadAll(r.Body)
-
-		if err != nil {
-			channel.Errors() <- err
-		}
-
-		input := hub.Input{
-			Payload: body,
-		}
-
-		channel.Out() <- input
-		w.WriteHeader(http.StatusAccepted)
-	})
+	http.HandleFunc("/", rootHandler(router))
 
 	go func() {
 		log.Fatal(http.ListenAndServe(binding, nil))
@@ -51,4 +28,38 @@ func NewHTTPChannel(uri string, router *router) Channel {
 
 	router.register(uri, channel)
 	return channel
+}
+
+func rootHandler(router *router) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		path := r.URL.Path
+
+		ok, channel := router.Match(path)
+
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		if r.Body == nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+
+		}
+		body, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			channel.Errors() <- err
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		input := hub.Input{
+			Payload: body,
+		}
+
+		channel.Out() <- input
+		w.WriteHeader(http.StatusAccepted)
+	}
 }
---
+++ pipe/http_test.go
@@ -0,0 +1,64 @@
+package pipe
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotConfiguredURIGet404(t *testing.T) {
+	t.Parallel()
+
+	router := DefaultRouter()
+
+	req, _ := http.NewRequest("POST", "/abc", nil)
+
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(rootHandler(router))
+
+	handler.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, http.StatusNotFound)
+
+}
+
+func TestConfiguredURINoContentGet400(t *testing.T) {
+	t.Parallel()
+
+	router := DefaultRouter()
+
+	NewHTTPChannel("/abc", router)
+
+	req, _ := http.NewRequest("POST", "/abc", nil)
+
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(rootHandler(router))
+
+	handler.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, http.StatusBadRequest)
+
+}
+
+func TestConfiguredURIContentGet202(t *testing.T) {
+	t.Parallel()
+
+	router := DefaultRouter()
+
+	channel := NewHTTPChannel("/abc", router)
+
+	// ensure the 'out' channel is emptied
+	go func() { <-channel.Out() }()
+
+	request := "{\"a\" : 1}"
+
+	req, _ := http.NewRequest("POST", "/abc", strings.NewReader(request))
+
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(rootHandler(router))
+
+	handler.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, http.StatusAccepted)
+
+}
