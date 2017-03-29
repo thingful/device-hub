4adf3bd7c255f87600b588a3295781c82cf57fc2
Mark deVilliers <markdevilliers@gmail.com>
2017-03-29 11:55:15 +0100

    correct comments

--- hub.go
+++ hub.go
@@ -6,13 +6,13 @@ type Listener interface {
 	Close() error
 }
 
-// Channel exposes errors and Input channels
+// Channel exposes errors and Message channels
 type Channel interface {
 	Errors() chan error
 	Out() chan Message
 }
 
-// Message contains a Payload and any Metadata collected
+// Message contains a Payload, a processed Output and any Metadata collected
 type Message struct {
 	Payload  []byte                 `json:"payload"`
 	Output   interface{}            `json:"output"`
