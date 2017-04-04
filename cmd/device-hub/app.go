cf99f6b766806362c11bf9bc9c2df991d2e32fbf
Mark deVilliers <markdevilliers@gmail.com>
2017-04-04 16:45:11 +0100

    extract out app from main

---
+++ cmd/device-hub/app.go
@@ -0,0 +1,155 @@
+// Copyright © 2017 thingful
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	hub "github.com/thingful/device-hub"
+	"github.com/thingful/device-hub/config"
+	"github.com/thingful/device-hub/endpoint"
+	"github.com/thingful/device-hub/engine"
+	"github.com/thingful/device-hub/listener"
+)
+
+type app struct {
+	config *config.Configuration
+}
+
+func NewHub(config *config.Configuration) app {
+
+	return app{
+		config: config,
+	}
+}
+
+func (a app) Run() (context.Context, error) {
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for _, pipe := range a.config.Pipes {
+
+		found, listenerConf := a.config.Listeners.FindByUID(pipe.Listener)
+
+		if !found {
+			return nil, fmt.Errorf("listener with UID %s not found", pipe.Listener)
+		}
+
+		endpoints := []hub.Endpoint{}
+
+		for e := range pipe.Endpoints {
+
+			found, endpointConf := a.config.Endpoints.FindByUID(pipe.Endpoints[e])
+
+			if !found {
+				return nil, fmt.Errorf("endpoint with UID %s not found", pipe.Endpoints[e])
+			}
+
+			if endpointConf.Type == "stdout" {
+
+				prettyPrint := endpointConf.Configuration.DBool("prettyPrint", false)
+
+				endpoints = append(endpoints, endpoint.NewStdOutEndpoint(prettyPrint))
+			}
+
+		}
+
+		found, profile := a.config.Profiles.FindByUID(pipe.Profile)
+
+		if !found {
+			return nil, fmt.Errorf("profile with UID %s not found", pipe.Profile)
+		}
+
+		listener, err := startListener(listenerConf, cancel)
+
+		if err != nil {
+			return nil, err
+		}
+
+		channel, err := listener.NewChannel(pipe.Uri)
+
+		if err != nil {
+			return nil, err
+		}
+
+		go startPipe(ctx, listener, channel, profile, endpoints)
+
+	}
+
+	return ctx, nil
+}
+
+func startPipe(ctx context.Context, listener hub.Listener, channel hub.Channel, profile config.Profile, endpoints []hub.Endpoint) {
+
+	scripter := engine.New()
+
+	for {
+
+		select {
+
+		case <-ctx.Done():
+			return
+
+		case err := <-channel.Errors():
+			log.Println(err)
+
+		case input := <-channel.Out():
+
+			output, err := scripter.Execute(profile.Script, input)
+
+			if err != nil {
+				log.Println(err)
+			}
+
+			output.Metadata[hub.PROFILE_NAME_KEY] = profile.Name
+			output.Metadata[hub.PROFILE_VERSION_KEY] = profile.Version
+			output.Metadata[hub.RUNTIME_VERSION_KEY] = SourceVersion
+
+			for e := range endpoints {
+
+				err = endpoints[e].Write(output)
+
+				if err != nil {
+					log.Println(err)
+				}
+
+			}
+		}
+
+	}
+}
+
+func startListener(endpoint config.Endpoint, cancel context.CancelFunc) (hub.Listener, error) {
+
+	if endpoint.Type == "std" {
+
+		return listener.NewStdInListener(cancel)
+	}
+	if endpoint.Type == "mqtt" {
+
+		clientName := fmt.Sprintf("device-hub-%s", SourceVersion)
+
+		brokerAddress := endpoint.Configuration.MString("MQTTBrokerAddress")
+
+		options := listener.DefaultMQTTOptions(brokerAddress, clientName)
+		client := listener.DefaultMQTTClient(options)
+
+		// TODO : set sensible wait time
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			exitWithError(token.Error())
+		}
+
+		return listener.NewMQTTListener(client)
+	}
+
+	if endpoint.Type == "http" {
+
+		binding := endpoint.Configuration.MString("HTTPBindingAddress")
+		return listener.NewHTTPListener(binding)
+	}
+
+	return nil, fmt.Errorf("listener of type %s not found", endpoint.Type)
+
+}
