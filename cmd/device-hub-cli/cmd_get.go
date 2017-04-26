f2389d6ea122ab50a8572aab794c851a838741ef
Mark deVilliers <markdevilliers@gmail.com>
2017-04-26 10:20:14 +0100

    remove dulplicate cmd

--- cmd/device-hub-cli/cmd_get.go
+++ cmd/device-hub-cli/cmd_get.go
@@ -40,39 +40,3 @@ var getCommand = &cobra.Command{
 		fmt.Println(args)
 	},
 }
-
-var createCommand = &cobra.Command{
-	Use:   "create",
-	Short: "Create listener and endpoint resources",
-	Run: func(cmd *cobra.Command, args []string) {
-
-		/* TODO : add ability to generate examplesi */
-		v := proto.CreateRequest{
-			Type: "listener",
-			Kind: "http",
-			Configuration: map[string]string{
-				"HTTPAddress": "0.0.0.0:8085",
-			},
-		}
-
-		err := roundTrip(v, func(cli proto.HubClient, in iocodec.Decoder, out iocodec.Encoder) error {
-
-			err := in.Decode(&v)
-			if err != nil {
-				return err
-			}
-
-			resp, err := cli.Create(context.Background(), &v)
-
-			if err != nil {
-				return err
-			}
-
-			return out.Encode(resp)
-
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-	},
-}
