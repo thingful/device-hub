bc0701a7cc64bf059267a99a0e53cd44f3019245
chespinoza <chespinoza@gmail.com>
2017-06-21 07:17:07 -0400

    update in the licenses command path
    -update in the licenses command target path
    -added -i flag to grep

--- scripts/check-vendor-license.sh
+++ scripts/check-vendor-license.sh
@@ -9,8 +9,8 @@ enabled_license="MIT\|APACHE"
 #enabled_license="MIT" 
 IFS=$'\n'
 
-for line in $(licenses -w ./../device-hub); do
-	if ! echo $line | awk '{ print $2 }' | grep -q $enabled_license; then
+for line in $(licenses ./../device-hub/cmd/device-hub); do
+	if ! echo $line | awk '{ print $2 }' | grep -qi $enabled_license; then
 		echo "${line} -> missing or wrong license"
 		ret=1
 	fi
