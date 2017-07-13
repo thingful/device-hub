4d89f5e5c408a2325fbd6bde71914f860ee77337
ChristianEspinoza <chespinoza@gmail.com>
2017-07-12 22:25:32 -0400

    add config file
    - add config file
    - naive approach

--- cmd/device-hub-cli/cmd_start.go
+++ cmd/device-hub-cli/cmd_start.go
@@ -8,12 +8,28 @@ import (
 	"fmt"
 	"strings"
 
+	"io/ioutil"
+	"log"
+
+	"os"
+
 	"github.com/fiorix/protoc-gen-cobra/iocodec"
 	"github.com/spf13/cobra"
 	"github.com/thingful/device-hub/proto"
+	"gopkg.in/yaml.v2"
 )
 
 func startCommand() *cobra.Command {
+
+	var configPath string
+	var configFile bool
+	type clientConfig struct {
+		URI          string   `yaml:"uri"`
+		Type         string   `yaml:"type"`
+		EndpointUIDs string   `yaml:"endpoint-uid"`
+		ListenerUID  string   `yaml:"listener-uid"`
+		Tags         []string `yaml:"tags"`
+	}
 
 	request := proto.StartRequest{
 		Endpoints: []string{},
@@ -59,11 +75,37 @@ func startCommand() *cobra.Command {
 		},
 	}
 
-	startCommand.Flags().StringVarP(&request.Listener, "listener", "l", request.Listener, "listener uid to accept messages on")
-	startCommand.Flags().StringVarP(&request.Uri, "uri", "u", request.Uri, "uri to listen on")
-	startCommand.Flags().StringSliceVarP(&request.Endpoints, "endpoint", "e", request.Endpoints, "endpoint uid to push messages to, may be specified multiple times")
-	startCommand.Flags().StringSliceVarP(&tags, "tags", "t", tags, "colon separated (k:v) runtime tags to attach to requests, may be specified multiple times")
+	startCommand.Flags().BoolVarP(&configFile, "config-file", "c", configFile, "enable config file feature")
+	startCommand.Flags().StringVar(&configPath, "config-path", configPath, "config file path with the required resources")
+	startCommand.ParseFlags(os.Args)
 
+	// fmt.Println("configFile:", configFile)
+	// TODO check type
+	if configFile {
+
+		readFields := clientConfig{}
+
+		content, err := ioutil.ReadFile(configPath)
+		if err != nil {
+			log.Fatalf("Failed to read config file [%s]: %s\n", configPath, err.Error())
+		}
+		err = yaml.Unmarshal(content, &readFields)
+		if err != nil {
+			log.Fatalf("Error parsing config file [%s]: %s\n", configPath, err.Error())
+		}
+		fmt.Println(readFields)
+		request.Listener = readFields.ListenerUID
+		request.Uri = readFields.URI
+		// TODO Parse string slice
+		//request.Endpoints = readFields.EndpointUIDs
+		tags = readFields.Tags
+
+	} else {
+		startCommand.Flags().StringVarP(&request.Listener, "listener", "l", request.Listener, "listener uid to accept messages on")
+		startCommand.Flags().StringVarP(&request.Uri, "uri", "u", request.Uri, "uri to listen on")
+		startCommand.Flags().StringSliceVarP(&request.Endpoints, "endpoint", "e", request.Endpoints, "endpoint uid to push messages to, may be specified multiple times")
+		startCommand.Flags().StringSliceVarP(&tags, "tags", "t", tags, "colon separated (k:v) runtime tags to attach to requests, may be specified multiple times")
+	}
 	return startCommand
 }
 
